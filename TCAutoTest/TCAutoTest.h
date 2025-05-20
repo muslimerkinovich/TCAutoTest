c7174baff41396cb1f325b8df0c3fd470dd295d3
muslimerkinovich <muslimerkinovich@gmail.com>
2025-05-20 14:42:10 +0500

    Initial Commit

---
+++ TCAutoTest/TCAutoTest.h
@@ -0,0 +1,18 @@
+//
+//  TCAutoTest.h
+//  TCAutoTest
+//
+//  Created by Muslim Erminjonov on 20/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TCAutoTest.
+FOUNDATION_EXPORT double TCAutoTestVersionNumber;
+
+//! Project version string for TCAutoTest.
+FOUNDATION_EXPORT const unsigned char TCAutoTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCAutoTest/PublicHeader.h>
+
+
